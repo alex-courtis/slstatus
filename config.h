daef71a467028108ad8eeecbd670c793d59d0817
Alexander Courtis <alex@courtis.org>
2023-10-24 11:24:50 +1100

    Revert "duke is the new gigantor"
    This reverts commit d7fcd0ecea5110b47cb7c43134f1d8a7b392f8e8.

--- config.h
+++ config.h
@@ -78,7 +78,7 @@ static const struct arg args[] = {
 #if defined(HOST_duke)
 	{ wifi_essid,			"%s",		"wlp0s20f3" },
 #elif defined(HOST_gigantor)
-	{ wifi_essid,			"%s",		"wlp0s20f3" },
+	{ wifi_essid,			"%s",		"wlp59s0" },
 #endif
 
 #if defined(HOST_duke)
