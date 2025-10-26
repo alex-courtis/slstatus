f8fb551077db98eea1d9851f9532c5d36a709221
Alexander Courtis <alex@courtis.org>
2025-10-26 13:16:26 +1100

    config.mk merge upstream

--- config.mk
+++ config.mk
@@ -11,10 +11,16 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # flags
-CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE -DHOST_$(shell hostname)
-CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Werror -Os -g
-LDFLAGS  = -L$(X11LIB)
-LDLIBS   = -lX11 -lsensors -lpulse -lm -lpthread
+CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\"
+CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wno-unused-parameter -Os
+LDFLAGS  = -L$(X11LIB) -s
+# OpenBSD: add -lsndio
+# FreeBSD: add -lkvm -lsndio
+LDLIBS   = -lX11
+
+# AMC
+CPPFLAGS += -DHOST_$(shell hostname)
+LDLIBS   += -lsensors -lpulse -lm -lpthread
 
 # compiler and linker
 CC = cc
